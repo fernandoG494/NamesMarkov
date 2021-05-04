941afeb56dc6dc57393fe3964677194016cf9dd4
CthulhuRise97 <42920098+CthulhuRise97@users.noreply.github.com>
2021-05-03 22:09:14 -0500

    Agregar archivos de proyecto.

---
+++ LinesProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NamesMarkov
+{
+    class LinesProvider
+    {
+        public LinesProvider()
+        {
+        }
+
+        public string[] Lines()
+        {
+            string basePath = AppContext.BaseDirectory;
+            string[] split = basePath.Split('\\');
+            string path = "";
+            for (int i = 0; i < split.Length - 4; i++)
+            {
+                path += split[i] + '\\';
+            }
+            
+            path += "Dictionary\\Draconic.txt";
+            Console.WriteLine(path);
+            string[] empty = { };
+            if (File.Exists(path))
+            {
+                return File.ReadAllLines(path);
+            }
+            else
+            {
+                Console.WriteLine("No existing file");
+                return empty;
+            }
+        }
+
+        public List<String> Convert(string[] arr)
+        {
+            List<string> list = new List<string>(arr);
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NamesMarkov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t AAAAAAAAAAAAAAAAAAAAAAAAAA!");
+            LinesProvider provider = new LinesProvider();
+            string[] lines = provider.Lines();
+
+            List<String> list = provider.Convert(lines);
+
+            var namegen = new Sobriquet.Generator(3, list);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(namegen.Next());
+            }
+        }
+    }
+}
