b0a9b0c46fa53c82ae1c3e3543dab8e447c2bb6d
CthulhuRise97 <42920098+CthulhuRise97@users.noreply.github.com>
2021-05-05 12:03:22 -0500

    Refactor
    Refactor in program

--- Program.cs
+++ Program.cs
@@ -8,13 +8,26 @@ namespace NamesMarkov
         static void Main(string[] args)
         {
             Console.WriteLine("\t Names Generator!");
+            
+            // numero de nombres
+            int n = 1;
+
+            // diccionario [Ancient_greek, Arachnid, Biblical, Dark_elven, Draconic, Egiptian, Elven, English, French, German, Giant, Orc, Punic, Serpent]
+            string origen = "Giant";
+
+            Starter(origen, n);
+            
+        }
+
+        static void Starter(string names, int n)
+        {
             LinesProvider provider = new LinesProvider();
-            string[] lines = provider.Lines("Biblical");
+            string[] lines = provider.Lines(names);
 
             List<String> list = provider.Convert(lines);
 
             var namegen = new Sobriquet.Generator(3, list);
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.WriteLine(namegen.Next());
             }
