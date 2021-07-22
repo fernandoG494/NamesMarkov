87d6df0b3871ee5e65cf3ef832d291990b4f22c1
Luis Fernando García Castañeda <lfgc851@gmail.com>
2021-07-22 00:26:52 -0500

    Mac update

--- LinesProvider.cs
+++ LinesProvider.cs
@@ -6,22 +6,34 @@ namespace NamesMarkov
 {
     class LinesProvider
     {
+        char separator;
         public LinesProvider()
         {
+            OperatingSystem os = System.Environment.OSVersion;
+            string plataform = os.Platform.ToString();
+            if (plataform == "Unix")
+            {
+                separator = '/';
+            }
+            else
+            {
+                separator = '\\';
+            }
         }
 
         public string[] Lines(string names)
         {
             string basePath = AppContext.BaseDirectory;
-            string[] split = basePath.Split('\\');
+            //Console.WriteLine("Base path: " + basePath);
+            string[] split = basePath.Split(separator);
             string path = "";
             for (int i = 0; i < split.Length - 4; i++)
             {
-                path += split[i] + '\\';
+                path += split[i] + separator;
             }
             
-            path += "Dictionary\\" + names + ".txt";
-            Console.WriteLine(path);
+            path += "Dictionary" + separator + names + ".txt";
+            //Console.WriteLine(path);
             string[] empty = { };
             if (File.Exists(path))
             {
